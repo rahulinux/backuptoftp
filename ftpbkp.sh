df653621bbc230fed9d07dbc51ad178463000cc3
Rahul Patil <loginrahul90@gmail.com>
2013-06-01 09:33:31 +0530

    Update ftpbkp.sh

--- ftpbkp.sh
+++ ftpbkp.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/evn bash
 #----------------------------------------------------------------------------
-# Name          :- ftpbkp.sh    v 0.1   Copyright (c) 20013-2014
-# Pupose        :- Uploading Backup files to FTP Server
-# Author        :- Rahul Patil<http://www.linuxian.com>
-# Created       :- 28 May 2013
-# Version       :- 0.1
-# License       :- GPL
+# Name :- ftpbkp.sh v 0.1 Copyright (c) 20013-2014
+# Pupose :- Uploading Backup files to FTP Server
+# Author :- Rahul Patil<http://www.linuxian.com>
+# Created :- 28 May 2013
+# Version :- 0.1
+# License :- free
 # Report `ftpbkp.sh` bugs to loginrahul90@gmail.com
 #----------------------------------------------------------------------------
 
@@ -14,17 +14,18 @@
 # This Script will first try to login with mention username password
 # in the script , if login fail then it will try to loing with anoymous
 # after login successfully it will upload file to ftp server
+# stored logs in /tmp/ftpbkp.log
 #----------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------
 # Variables=
 #----------------------------------------------------------------------------
-Uname=''                        # Set ftp Username
-Passwd=''                       # Set ftp password
-Ftp_Server='192.168.226.1'      # Ftp Server IP
+Uname='' # Set ftp Username
+Passwd='' # Set ftp password
+Ftp_Server='' # Ftp Server IP
 
-: ${Uname:=anonymous}   # if username is not define then make it anonymous
-: ${Passwd:=anonymous}  # if Password is not define then make it anonymous
+: ${Uname:=anonymous} # if username is not define then make it anonymous
+: ${Passwd:=anonymous} # if Password is not define then make it anonymous
 
 
 #----------------------------------------------------------------------------
@@ -36,20 +37,31 @@ Usage: $0 [OPTION]... [FILE]...
 
 Mandatory arguments to long options are mandatory for short options too.
 
-  -f, --file  <filename1...>         Input files which you want to upload
+-f, --file <filename1...> Input files which you want to upload
 
 Report $0 bugs to loginrahul90@gmail.com
 _EOF
 }
 
+check_connection(){
+
+  if [[ -z $Ftp_Server ]]; then
+		 echo "Please Specify FTP server in `basename $0`"
+	elif ! nc -vzw2 ${Ftp_Server} 21 >/dev/null; then
+		 echo "Unable to connect $Ftp_Server"
+		 exit 1
+	fi
+  
+}
+
 Upload_files() {
 
 ftp -inv $Ftp_Server <<-_EOF
-        user $Uname $Passwd
-        bin  # Enable Binary Mode
-        mput ${data[@]:1}
-        bye
-        _EOF
+user $Uname $Passwd
+bin # Enable Binary Mode
+mput ${data[@]:1}
+bye
+_EOF
 
 }
 
@@ -58,7 +70,13 @@ Main(){
 
         case $1 in
                 -f) data=( $* )
-                    Upload_files
+					         {
+					           echo "---@@@--{ Ftp Backup Start at $( date ) }--@@@---"
+					           check_connection
+					           Upload_files  
+					           echo "---@@@--{ Ftp Backup End at $( date ) }--@@@---"
+					         } tee -a /tmp/ftpbkp.log
+					
                     ;;
                  *) Show_Help
                     ;;
